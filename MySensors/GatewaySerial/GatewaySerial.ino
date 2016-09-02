69f5fecef0f86f89a414044ae1f6a2ba382b2672
Uladzimir Kazakevich <uladzimir@kazakevich.org>
2016-09-02 14:29:09 +0300

    add arduino sensors

---
+++ MySensors/GatewaySerial/GatewaySerial.ino
@@ -0,0 +1,155 @@
+  /**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * DESCRIPTION
+ * The ArduinoGateway prints data received from sensors on the serial link. 
+ * The gateway accepts input on seral which will be sent out on radio network.
+ *
+ * The GW code is designed for Arduino Nano 328p / 16MHz
+ *
+ * Wire connections (OPTIONAL):
+ * - Inclusion button should be connected between digital pin 3 and GND  
+ * - RX/TX/ERR leds need to be connected between +5V (anode) and digital pin 6/5/4 with resistor 270-330R in a series
+ *
+ * LEDs (OPTIONAL):
+ * - To use the feature, uncomment MY_LEDS_BLINKING_FEATURE in MyConfig.h
+ * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
+ * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
+ * - ERR (red) - fast blink on error during transmission error or recieve crc error 
+ * 
+ */
+
+// Enable debug prints to serial monitor
+//#define MY_DEBUG 
+//#define MY_DEBUG_VERBOSE_SIGNING
+//#define MY_DEBUG_VERBOSE_RF24
+
+//#define MY_SIGNING_FEATURE
+//#define MY_SIGNING_SOFT
+//#define MY_SIGNING_REQUEST_SIGNATURES
+//#define MY_RF24_ENABLE_ENCRYPTION
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+// Set LOW transmit power level as default, if you have an amplified NRF-module and
+// power your radio separately with a good regulator you can turn up PA level. 
+#define MY_RF24_PA_LEVEL RF24_PA_LOW
+
+// Enable serial gateway
+#define MY_GATEWAY_SERIAL
+
+// Define a lower baud rate for Arduino's running on 8 MHz (Arduino Pro Mini 3.3V & SenseBender)
+#if F_CPU == 8000000L
+#define MY_BAUD_RATE 38400
+#endif
+
+// Flash leds on rx/tx/err
+#define MY_LEDS_BLINKING_FEATURE
+// Set blinking period
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
+
+// Inverses the behavior of leds
+//#define MY_WITH_LEDS_BLINKING_INVERSE
+
+// Enable inclusion mode
+//#define MY_INCLUSION_MODE_FEATURE
+// Enable Inclusion mode button on gateway
+//#define MY_INCLUSION_BUTTON_FEATURE
+
+// Inverses behavior of inclusion button (if using external pullup)
+//#define MY_INCLUSION_BUTTON_EXTERNAL_PULLUP
+
+// Set inclusion mode duration (in seconds)
+#define MY_INCLUSION_MODE_DURATION 60 
+// Digital pin used for inclusion mode button
+#define MY_INCLUSION_MODE_BUTTON_PIN  3 
+
+// Uncomment to override default HW configurations
+//#define MY_DEFAULT_ERR_LED_PIN 4  // Error led pin
+//#define MY_DEFAULT_RX_LED_PIN  6  // Receive led pin
+//#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
+
+#include <SPI.h>
+#include <MySensors.h>  
+#include <DHT.h>  
+
+#define CHILD_ID_HUM 0
+#define CHILD_ID_TEMP 1
+#define HUMIDITY_SENSOR_DIGITAL_PIN 3
+
+DHT dht;
+float lastTemp;
+float lastHum;
+boolean metric = true; 
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+const long interval = 30000;              // interval at which to read (milliseconds)
+
+void setup() { 
+  // Setup locally attached sensors
+  dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN); 
+  metric = getConfig().isMetric;
+}
+
+void presentation() {
+  // Send the Sketch Version Information to the Gateway
+  sendSketchInfo("Garage Gateway", "1.0");
+
+  // Register all sensors to gw (they will be created as child devices)
+  present(CHILD_ID_HUM, S_HUM);
+  present(CHILD_ID_TEMP, S_TEMP);
+}
+
+void loop() { 
+  wait(dht.getMinimumSamplingPeriod());
+ 
+  // Fetch temperatures from DHT sensor
+  float temperature = dht.getTemperature();
+  if (isnan(temperature)) {
+      Serial.println("Failed reading temperature from DHT");
+  } else if (temperature != lastTemp) {
+    lastTemp = temperature;
+    if (!metric) {
+      temperature = dht.toFahrenheit(temperature);
+    }
+    send(msgTemp.set(temperature, 1));
+    #ifdef MY_DEBUG
+    Serial.print("T: ");
+    Serial.println(temperature);
+    #endif
+  }
+  
+  // Fetch humidity from DHT sensor
+  float humidity = dht.getHumidity();
+  if (isnan(humidity)) {
+      Serial.println("Failed reading humidity from DHT");
+  } else if (humidity != lastHum) {
+      lastHum = humidity;
+      send(msgHum.set(humidity, 1));
+      #ifdef MY_DEBUG
+      Serial.print("H: ");
+      Serial.println(humidity);
+      #endif
+  }
+
+  wait(interval);
+}
---
+++ MySensors/KitchenSensor/KitchenSensor.ino
@@ -0,0 +1,193 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik EKblad
+ * 
+ * DESCRIPTION
+ * This sketch provides an example how to implement a humidity/temperature
+ * sensor using DHT11/DHT-22 
+ * http://www.mysensors.org/build/humidity
+ */
+ 
+// Enable debug prints
+//#define MY_DEBUG
+//#define MY_SENROS_DEBUG
+
+#define MY_NODE_ID 2
+#define MY_REPEATER_FEATURE
+
+//#define MY_SIGNING_FEATURE
+//#define MY_SIGNING_SOFT
+//#define MY_SIGNING_REQUEST_SIGNATURES
+//#define MY_RF24_ENABLE_ENCRYPTION
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+#include <SPI.h>
+#include <MySensors.h>
+#include <DHT.h>  
+#include <RCSwitch.h>
+#include <NewPing.h>
+
+#define CHILD_ID_HUM 0
+#define CHILD_ID_TEMP 1
+#define CHILD_ID_DISTANCE 2
+#define CHILD_ID_LIGHT 3
+#define HUMIDITY_SENSOR_DIGITAL_PIN 4
+#define TRANSMITTER_433MHZ_DIGITAL_PIN 5
+#define DISTANCE_SENSOR_ECHO_PIN 6
+#define DISTANCE_SENSOR_TRIGGER_PIN 7
+#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define DISTANCE_SENSOR_SLEEP_TIME 50
+const long interval = 30000;              // interval at which to read (milliseconds)
+
+/*#define LIGHT_ONOFF "111111100000100110000111"
+#define LIGHT_BRIGHT_PLUS "111111100000100110000011"
+#define LIGHT_BRIGHT_MINUS "111111100000100110000010"*/
+
+DHT dht;
+float lastTemp;
+float lastHum;
+
+NewPing sonar(DISTANCE_SENSOR_TRIGGER_PIN, DISTANCE_SENSOR_ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+int lastDist;
+int lastSleep;
+
+boolean metric = true; 
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+MyMessage msgDistance(CHILD_ID_DISTANCE, V_DISTANCE);
+MyMessage msgLight(CHILD_ID_LIGHT, V_VAR1);
+RCSwitch mySwitch = RCSwitch();
+
+void setup()  
+{ 
+  dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN); 
+  
+  // 433Mhz Transmitter
+  mySwitch.enableTransmit(TRANSMITTER_433MHZ_DIGITAL_PIN);
+  mySwitch.setProtocol(1);
+  mySwitch.setPulseLength(201);
+  //mySwitch.setRepeatTransmit(3);
+
+  metric = getConfig().isMetric;
+  lastSleep = 0;
+
+  // request last state
+  // request(CHILD_ID_LIGHT, V_VAR1);
+}
+
+void presentation()  
+{ 
+  // Send the Sketch Version Information to the Gateway
+  sendSketchInfo("Kitchen 1st fl", "1.0");
+
+  // Register all sensors to gw (they will be created as child devices)
+  present(CHILD_ID_HUM, S_HUM);
+  present(CHILD_ID_TEMP, S_TEMP);
+  present(CHILD_ID_DISTANCE, S_DISTANCE);
+  present(CHILD_ID_LIGHT, S_CUSTOM);
+}
+
+void loop()      
+{  
+  wait(DISTANCE_SENSOR_SLEEP_TIME);
+
+  // read distance data
+  int dist = metric ? sonar.ping_cm() : sonar.ping_in(); 
+  if (dist != lastDist) {
+      #ifdef MY_SENROS_DEBUG
+      Serial.print("Ping: ");
+      Serial.print(dist);
+      Serial.println(metric ? " cm" : " in");
+      #endif
+
+      if (dist < 20 && lastDist > 30){
+        // Serial.println("LIGHTS ON/OFF");
+         mySwitch.send("111111100000100110000111");
+         send(msgDistance.set(dist, 1));
+      }
+      
+      lastDist = dist; 
+  }
+
+  lastSleep += DISTANCE_SENSOR_SLEEP_TIME;
+
+  if (lastSleep < dht.getMinimumSamplingPeriod()+interval){
+    return;
+  }
+     
+  // Fetch temperatures from DHT sensor
+  float temperature = dht.getTemperature();
+  if (isnan(temperature)) {
+    #ifdef MY_SENROS_DEBUG
+      Serial.println("Failed reading temperature from DHT");
+    #endif
+  } else if (temperature != lastTemp) {
+    lastTemp = temperature;
+    if (!metric) {
+      temperature = dht.toFahrenheit(temperature);
+    }
+    send(msgTemp.set(temperature, 1));
+    #ifdef MY_SENROS_DEBUG
+    Serial.print("T: ");
+    Serial.println(temperature);
+    #endif
+  }
+  
+  // Fetch humidity from DHT sensor
+  float humidity = dht.getHumidity();
+  if (isnan(humidity)) {
+    #ifdef MY_SENROS_DEBUG
+      Serial.println("Failed reading humidity from DHT");
+    #endif
+  } else if (humidity != lastHum) {
+      lastHum = humidity;
+      send(msgHum.set(humidity, 1));
+      #ifdef MY_SENROS_DEBUG
+      Serial.print("H: ");
+      Serial.println(humidity);
+      #endif
+  }
+
+  //sleep(INTERRUPT,CHANGE, SLEEP_TIME); //sleep a bit
+}
+
+void receive(const MyMessage &message) {
+  #ifdef MY_SENROS_DEBUG
+  Serial.print("Message received. type: ");
+  Serial.print(message.type);
+  Serial.print(" data: ");
+  Serial.println(message.data);
+  #endif
+  
+  if (message.type==V_VAR1) { 
+     unsigned long cmd = message.getULong();
+     #ifdef MY_SENROS_DEBUG
+     Serial.print("RF Received from Net: ");
+     Serial.println(cmd);
+     #endif
+     mySwitch.send(cmd, 24);
+     //mySwitch.send("111111100000100110000111");
+   }
+}
